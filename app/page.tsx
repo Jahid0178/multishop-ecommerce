dade3566380214101dae3c9345ce03ec3fa26c65
Jahid0178 <zahidul.haque767@gmail.com>
2023-06-26 01:13:51 +0600

    add - old company products data

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,23 @@ const HomePage = () => {
           </Box>
         </Container>
       </Box>
+
+      <Box component="section">
+        <Container size="lg">
+          <Box component="div">
+            <Title order={2}>Old Company Products</Title>
+            <Box component="div" mt={20}>
+              <Grid>
+                {productsData.map((product) => (
+                  <Grid.Col key={product.id} span={12} md={6} lg={3}>
+                    <ProductsCard data={product} />
+                  </Grid.Col>
+                ))}
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 };
