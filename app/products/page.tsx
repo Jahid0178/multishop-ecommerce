cb9d4fccf26d841371fce684ae315a1b2eaa796d
Jahid0178 <zahidul.haque767@gmail.com>
2023-06-26 15:08:01 +0600

    mod - products showing in best selling page and products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,30 @@
 "use client";
 
+import { productsData } from "@/data/data";
+import ProductsCard from "@/ui/Cards";
+import { Box, Container, Grid } from "@mantine/core";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <Box component="section">
+      <Container size="lg">
+        <Grid>
+          <Grid.Col span={12} md={3}>
+            Sidebar Section
+          </Grid.Col>
+          <Grid.Col span={12} md={9}>
+            <Grid>
+              {productsData.map((product) => (
+                <Grid.Col span={6} md={4} key={product.id}>
+                  <ProductsCard data={product} />
+                </Grid.Col>
+              ))}
+            </Grid>
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default ProductsPage;
