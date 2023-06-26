0c169108ba8cb661740e12028a50099e1b30d619
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-06-27 00:58:08 +0600

    add auth page

---
+++ ui/Form/SignIn.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  PasswordInput,
+  TextInput,
+  Text,
+  Checkbox,
+} from "@mantine/core";
+import { useHover } from "@mantine/hooks";
+
+import { useForm } from "@mantine/form";
+const SignIn = ({ isSignUp }: { isSignUp: () => void }) => {
+  const { hovered, ref } = useHover();
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "secret",
+    },
+
+    // functions will be used to validate values at corresponding key
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value: any, values: { password: any }) =>
+        value !== values.password ? "Passwords did not match" : null,
+    },
+  });
+  return (
+    <Card>
+      <Box mx="auto">
+        <Text
+          style={{
+            fontSize: "50px",
+            fontWeight: "800",
+            color: "#2D385E",
+            textAlign: "center",
+          }}
+        >
+          MultyShop
+        </Text>
+        <Text
+          style={{
+            fontSize: "30px",
+            fontWeight: "600",
+            color: "#2D385E",
+            textAlign: "center",
+          }}
+        >
+          Login to your account
+        </Text>
+        <Text
+          onClick={isSignUp}
+          style={{
+            textAlign: "center",
+            marginTop: "11px",
+          }}
+        >
+          Don’t have an account?{" "}
+          <span
+            ref={ref}
+            style={
+              hovered
+                ? {
+                    cursor: "pointer",
+                    color: "#6382FE",
+                    textDecoration: "underline",
+                  }
+                : {}
+            }
+          >
+            Sign Up Free!
+          </span>
+        </Text>
+      </Box>
+      <Box mx="auto">
+        <form onSubmit={form.onSubmit(console.log)}>
+          <TextInput
+            mt="sm"
+            label="Email"
+            placeholder="Email"
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Password"
+            {...form.getInputProps("password")}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "11px",
+            }}
+          >
+            <Checkbox sx={{ color: "#959595" }} label="Remember me" />
+            <Text
+              style={{
+                cursor: "pointer",
+                color: "#549CF9",
+                textDecoration: "underline",
+              }}
+            >
+              Forgot password?
+            </Text>
+          </Box>
+          <Button size="lg" fullWidth type="submit" mt="sm">
+            Sign up{" "}
+          </Button>
+        </form>
+      </Box>
+    </Card>
+  );
+};
+
+export default SignIn;
