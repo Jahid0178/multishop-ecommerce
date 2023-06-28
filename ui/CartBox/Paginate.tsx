876d27f89aa4e417900d329afabc7eff8555d5b5
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-06-28 14:35:36 +0600

    cart functionality added

--- ui/CartBox/Paginate.tsx
+++ ui/CartBox/Paginate.tsx
@@ -1,48 +1,58 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useState } from "react";
-import { Box, Grid, Pagination, Image, Paper } from "@mantine/core";
+import {
+  Box,
+  Grid,
+  Pagination,
+  Image,
+  Paper,
+  Button,
+  NumberInput,
+  rem,
+  ActionIcon,
+} from "@mantine/core";
 import { paginateData, productsData } from "@/data/data";
+import React from "react";
+import { ProductType } from "@/libs/types/types";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 interface PaginationProps {
   totalItems: number;
   itemsPerPage: number;
+  state: React.Dispatch<React.SetStateAction<ProductType[]>>;
 }
 const PaginationD: React.FC<PaginationProps> = ({
   totalItems,
   itemsPerPage,
+  state,
 }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const { cartItems } = useSelector((state: RootState) => state.shoppingCart);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const totalPage = Math.ceil(paginateData.length / itemsPerPage);
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
   const indexOfLast = currentPage * itemsPerPage;
   const indexOfFirst = indexOfLast - itemsPerPage;
-  const data = paginateData.slice(indexOfFirst, indexOfLast);
+
+  const data = React.useCallback(() => {
+    const data = cartItems.slice(indexOfFirst, indexOfLast);
+    state(data);
+  }, [cartItems, indexOfFirst, indexOfLast, state]);
+
+  // console.log(ooo());
+  React.useEffect(() => {
+    data();
+  }, [data]);
 
   return (
-    <Box sx={{ height: "90%" }}>
-      <Grid>
-        {/* <Grid.Col span={4}>2</Grid.Col>
-        <Grid.Col span={4}>3</Grid.Col> */}
-        {data.map((data, index) => {
-          return (
-            <Grid.Col key={index} span={12} md={12}>
-              <Box sx={{ display: "flex" }}>
-                {data.title}
-                <Image width={200} height={80} fit="contain" src={data.src} />
-              </Box>
-            </Grid.Col>
-          );
-        })}
-      </Grid>
+    <Box sx={{ marginBottom: "20px" }}>
+      <Button size="lg" style={{ margin: "10px 0px" }} fullWidth>
+        Procced To cart
+      </Button>
 
-      <Box sx={{ position: "absolute", bottom: "7px" }}>
-        <Pagination
-          // initialPage={currentPage}
-          total={totalPage}
-          onChange={handlePageChange}
-        />
+      <Box>
+        <Pagination total={totalPage} onChange={handlePageChange} />
       </Box>
     </Box>
   );
