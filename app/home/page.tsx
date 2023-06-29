6637171c7e93b262c1084246f477a24d4f34ba30
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-06-29 15:28:22 +0600

    handle global erorr, refactor products page and add public products page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -21,6 +21,7 @@ import ContactModal from "@/ui/Modals/ContactModal";
 import { useDisclosure } from "@mantine/hooks";
 import CompanyProducts from "@/ui/CompanyProducts";
 import { products } from "@/libs/Data";
+import PublicProducts from "@/ui/PublicProducts";
 
 const useStyles = createStyles((theme) => ({
   root: {
@@ -146,6 +147,7 @@ const HomePage = () => {
         </div>
       </Box>
       <CompanyProducts products={products} />
+      <PublicProducts products={products} />
       {/* 
       <Box component="section">
         <Container size="lg">
