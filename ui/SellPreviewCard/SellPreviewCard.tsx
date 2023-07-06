7ae33e406b0e0baeb828e144107bb9a38784563f
Mostofa321 <hmshumon123@gmail.com>
2023-07-06 18:55:29 +0600

    Mod - midified top navbar & sell per view card & faq

--- ui/SellPreviewCard/SellPreviewCard.tsx
+++ ui/SellPreviewCard/SellPreviewCard.tsx
@@ -10,13 +10,13 @@ type SellPreviewCardProps = {
 
 const useStyles = createStyles((theme) => ({
   SellPreviewCard: {
-    background: "#EDF2FD",
-    boxShadow: "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px",
-    // Use pseudo-classes
-    '&:hover': {
-      background: "white",
-      boxShadow: "rgba(0, 0, 0, 0.2) 0px 60px 40px -7px",
-    },
+    background: "#3498DA",
+    color: "white",
+    // // Use pseudo-classes
+    // '&:hover': {
+    //   background: "white",
+    //   boxShadow: "rgba(0, 0, 0, 0.2) 0px 20px 30px",
+    // },
   },
 }));
 
