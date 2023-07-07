0b4302095b19f72096a34b94225f48b95b33da80
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-07-07 18:02:42 +0600

    add verify page

--- app/(user)/verifyEmail/page.tsx
+++ app/(user)/verifyEmail/page.tsx
@@ -1,45 +1,97 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { IconDatabase, IconAt } from "@tabler/icons-react";
-import { Button, Card, Input } from "@mantine/core";
+import { Box, Button, Card, Input, Text } from "@mantine/core";
+import React from "react";
 const VerifyEmailPage = () => {
+  const [error, setError] = React.useState<string>("");
   const searchParams = useSearchParams();
   //   const { token } = router.query;/
   const token = searchParams.get("token");
   const email = searchParams.get("email");
 
   // Implement the logic for verifying the email using the token
+  React.useEffect(() => {
+    const verify = async () => {
+      const response = await fetch(
+        `${process.env.BASE_URL}/api/auth/verifyEmail`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, token }), // Replace 'data' with your request payload
+        }
+      );
 
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const responseData = await response.json();
+      return responseData;
+    };
+    const data = verify();
+    console.log("data", data);
+  }, [token, email]);
   return (
-    <div style={{ width: "35%", margin: "0 auto" }}>
+    <Box
+      style={{
+        width: "35%",
+        margin: "0 auto",
+        padding: "20px 0px",
+        position: "relative",
+      }}
+    >
       {/* Add your email verification logic and UI components */}
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <h1>Verify Email</h1>
-        {/* <p>Token: {token}</p>
-        <p>email: {email}</p> */}
-        <Input
-          icon={<IconAt />}
-          variant="filled"
-          placeholder={email ? email : "email"}
-          disabled
-        />
-        <Input component="button">
-          <Input.Placeholder>Token:{token}</Input.Placeholder>
-        </Input>
-        <Input component="button">
-          <Input.Placeholder>Email:{email}</Input.Placeholder>
-        </Input>
-        <Button
-          variant="gradient"
-          gradient={{ from: "teal", to: "blue", deg: 60 }}
-          size="md"
-          leftIcon={<IconDatabase size="1rem" />}
-          loading
+      <Card
+        // style={{ height: "40vh" }}
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: "-15px",
+            fontSize: "40px",
+            fontWeight: "500",
+          }}
         >
-          Wait for verify
-        </Button>
+          Verify Email
+        </Text>
+
+        <Input.Wrapper
+          id="input-demo"
+          withAsterisk
+          label="Wait for verification"
+          error={error}
+        >
+          <Input
+            id="input-demo"
+            placeholder={email ? email : "email"}
+            disabled
+          />
+          <Input
+            style={{ margin: "8px 0px" }}
+            id="input-demo"
+            placeholder={token ? token : "token"}
+            disabled
+          />
+        </Input.Wrapper>
+        <Box style={{ textAlign: "center", marginTop: "10px" }}>
+          <Button
+            variant="gradient"
+            gradient={{ from: "teal", to: "blue", deg: 60 }}
+            size="md"
+            leftIcon={<IconDatabase size="1rem" />}
+            loading
+          >
+            Wait for verify
+          </Button>
+        </Box>
       </Card>
-    </div>
+    </Box>
   );
 };
 
